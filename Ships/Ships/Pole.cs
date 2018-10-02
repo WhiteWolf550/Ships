02d21ed1169fe4520200b6000792d9b2d15825af
Rosta Viskot <viskot007@gmail.com>
2018-10-02 16:14:37 +0200

    nnwer

--- Ships/Ships/Pole.cs
+++ Ships/Ships/Pole.cs
@@ -8,7 +8,14 @@ namespace Ships {
     class Pole {
         public int size = 10;
         public string state = "L";
-        public string state2 = "X";
+        bool Player1hlp1 = false;
+        bool Player1hlp2 = false;
+        bool Player1hlp3 = false;
+
+        bool Player2hlp1 = false;
+        bool Player2hlp2 = false;
+        bool Player2hlp3 = false;
+        string hitmes = "";
         public string[,] plocha = new string[10, 10];
         public void BaseSea()
         {
@@ -25,19 +32,62 @@ namespace Ships {
                 Console.WriteLine();
             }
         }
-        public void EnemyAttack(int hsy, int hsx, int ty, int tx, int t2y, int t2x, int t3y, int t3x) {
+
+        //KDYŽ ÚTOČÍ PLAYER2..............
+        public void Player2Attack(int hsy, int hsx, int ty, int tx, int t2y, int t2x, int t3y, int t3x) {
             if (hsy == tx && hsx == ty) {
                 plocha[ty, tx] = "X";
+                hitmes = "HIT!";
+                Player1hlp1 = true;
+                //Console.ForegroundColor = ConsoleColor.Red;
                 //Console.WriteLine(ty + tx + hsy + hsy);  ///////////////Pozor!
             }else if(hsy == t2y && hsx == t2x) {
                 plocha[t2x, t2y] = "X";
+                hitmes = "HIT!";
+                Player1hlp2 = true;
+                
             } else if (hsy == t3y && hsx == t3x) {
                 plocha[t3x, t3y] = "X";
+                hitmes = "HIT!";
+                Player1hlp3 = true;
+            }else {
+                hitmes = "MISS!";
+            }
+
+            
+        }
+
+        //KDYŽ ÚTOČÍ PLAYER1.........
+        public void Player1Attack(int hsy, int hsx, int ty, int tx, int t2y, int t2x, int t3y, int t3x) {
+            if (hsy == tx && hsx == ty) {
+                plocha[ty, tx] = "X";
+                hitmes = "HIT!";
+                Player2hlp1 = true;
+                //Console.ForegroundColor = ConsoleColor.Red;
+                //Console.WriteLine(ty + tx + hsy + hsy);  ///////////////Pozor!
+            }
+            else if (hsy == t2y && hsx == t2x) {
+                plocha[t2x, t2y] = "X";
+                hitmes = "HIT!";
+                Player2hlp2 = true;
+
+            }
+            else if (hsy == t3y && hsx == t3x) {
+                plocha[t3x, t3y] = "X";
+                hitmes = "HIT!";
+                Player2hlp3 = true;
+            }
+            else {
+                hitmes = "MISS!";
             }
         }
-        public void Sea(int xs, int ys, int xe, int ye, int xt, int yt)
+            
+            //POLE PLAYER1.......
+            public void Player1Sea(int xs, int ys, int xe, int ye, int xt, int yt)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(" 0 1 2 3 4 5 6 7 8 9");
+            Console.ForegroundColor = ConsoleColor.White;
             for (int x = 0; x < plocha.GetLength(0); x++)
             {
 
@@ -48,13 +98,53 @@ namespace Ships {
                     plocha[ys, xs] = "O";
                     plocha[xe, ye] = "O";
                     plocha[xt, yt] = "O";
+                    if (Player1hlp1 == true) {
+                        plocha[ys, xs] = "X";
+                    }
+                    if (Player1hlp2 == true) {
+                        plocha[ye, xe] = "X";
+                    }
+                    if (Player1hlp3 == true) {
+                        plocha[yt, xt] = "X";
+                    }
                     Console.Write(" " + plocha[y, x]);
                 }
                 Console.WriteLine();
             }
         }
-        public void Seaattack(int hy, int hx, int yrs, int xrs, int ys, int xs, int xe, int ye, int xt, int yt) {
+
+        //POLE PLAYER2..............
+        public void Player2Sea(int xs, int ys, int xe, int ye, int xt, int yt) {
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(" 0 1 2 3 4 5 6 7 8 9");
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int x = 0; x < plocha.GetLength(0); x++) {
+
+                for (int y = 0; y < plocha.GetLength(1); y++) {
+
+                    plocha[y, x] = state;
+                    plocha[ys, xs] = "O";
+                    plocha[xe, ye] = "O";
+                    plocha[xt, yt] = "O";
+                    if (Player2hlp1 == true) {
+                        plocha[ys, xs] = "X";
+                    }
+                    if (Player2hlp2 == true) {
+                        plocha[ye, xe] = "X";
+                    }
+                    if (Player2hlp3 == true) {
+                        plocha[yt, xt] = "X";
+                    }
+                    Console.Write(" " + plocha[y, x]);
+                }
+                Console.WriteLine();
+            }
+        }
+        //POLE PLAYER1 PO ÚTOKU.........
+        public void Player1Seaattack(int hy, int hx, int yrs, int xrs, int ys, int xs, int xe, int ye, int xt, int yt) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" 0 1 2 3 4 5 6 7 8 9");
+            Console.ForegroundColor = ConsoleColor.White;
             for (int x = 0; x < plocha.GetLength(0); x++) {
 
                 for (int y = 0; y < plocha.GetLength(1); y++) {
@@ -63,11 +153,53 @@ namespace Ships {
                     plocha[ys, xs] = "O";
                     plocha[ye, xe] = "O";
                     plocha[yt, xt] = "O";
-                    EnemyAttack(hy, hx, yrs, xrs, xe, ye, xt, yt);
+                    if (Player1hlp1 == true) {
+                        plocha[ys, xs] = "X";
+                    }
+                    if (Player1hlp2 == true) {
+                        plocha[ye, xe] = "X";
+                    }
+                    if(Player1hlp3 == true) {
+                        plocha[yt, xt] = "X";
+                    }
+                    Player2Attack(hy, hx, yrs, xrs, xe, ye, xt, yt);
                     Console.Write(" " + plocha[y, x]);
                 }
                 Console.WriteLine();
+                
             }
+            Console.WriteLine(hitmes);
+        }
+
+        //POLE PLAYER2 PO ÚTOKU................
+        public void Player2Seaattack(int hy, int hx, int yrs, int xrs, int ys, int xs, int xe, int ye, int xt, int yt) {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" 0 1 2 3 4 5 6 7 8 9");
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int x = 0; x < plocha.GetLength(0); x++) {
+
+                for (int y = 0; y < plocha.GetLength(1); y++) {
+
+                    plocha[y, x] = state;
+                    plocha[ys, xs] = "O";
+                    plocha[ye, xe] = "O";
+                    plocha[yt, xt] = "O";
+                    if (Player2hlp1 == true) {
+                        plocha[ys, xs] = "X";
+                    }
+                    if (Player2hlp2 == true) {
+                        plocha[ye, xe] = "X";
+                    }
+                    if (Player2hlp3 == true) {
+                        plocha[yt, xt] = "X";
+                    }
+                    Player1Attack(hy, hx, yrs, xrs, xe, ye, xt, yt);
+                    Console.Write(" " + plocha[y, x]);
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine(hitmes);
         }
     }
 }
--- Ships/Ships/Program.cs
+++ Ships/Ships/Program.cs
@@ -15,52 +15,74 @@ namespace Ships {
             string ys;
             string hitxs;
             string hitys;
-            Console.WriteLine("Pole Player1:");
-            pole.BaseSea();
+            //Console.WriteLine("Pole Player1:");
+            //pole.BaseSea();
+            //Console.ReadKey();
             Random rn = new Random();
-            int rn1x = rn.Next(0, 10);
-            int rn1y = rn.Next(0, 10);
-            int rn2x = rn.Next(0, 10);
-            int rn2y = rn.Next(0, 10);
-            int rn3x = rn.Next(0, 10);
-            int rn3y = rn.Next(0, 10);
-            int rn4x = rn.Next(0, 10);
-            int rn4y = rn.Next(0, 10);
-            int rn5x = rn.Next(0, 10);
-            int rn5y = rn.Next(0, 10);
+
+            //RANDOM PLAYER2
+            int player1rn1x = rn.Next(0, 10);
+            int player1rn1y = rn.Next(0, 10);
+            int player1rn2x = rn.Next(0, 10);
+            int player1rn2y = rn.Next(0, 10);
+            int player1rn3x = rn.Next(0, 10);
+            int player1rn3y = rn.Next(0, 10);
+
+            //RANDOM PLAYER2
+            int player2rn1x = rn.Next(0, 10);
+            int player2rn1y = rn.Next(0, 10);
+            int player2rn2x = rn.Next(0, 10);
+            int player2rn2y = rn.Next(0, 10);
+            int player2rn3x = rn.Next(0, 10);
+            int player2rn3y = rn.Next(0, 10);
             Console.WriteLine(" ");
-            pole.Sea(rn1x, rn1y, rn2x, rn2y, rn3x, rn3y);
+            /*Console.WriteLine("Pole Player1: ");
+            pole.Player1Sea(player1rn1x, player1rn1y, player1rn2x, player1rn2y, player1rn3x, player1rn3y);
+            Console.WriteLine("Pole Player2: ");
+            pole.Player2Sea(player2rn1x, player2rn1y, player2rn2x, player2rn2y, player2rn3x, player2rn3y);*/
             do {
                 
-                /*Console.WriteLine("Zadej X:");
-                xs = Console.ReadLine();
-                Console.WriteLine("Zadej Y:");
-                ys = Console.ReadLine();
-                int xr = Convert.ToInt32(xs);
-                int yr = Convert.ToInt32(ys);*/
-                //Console.WriteLine("xx" + xs + "xx");
-                //Console.WriteLine("yy" + yr + "yy");
+               
                 Console.WriteLine("");
-                // pole.state = "O";
-                Console.WriteLine(rn1x + rn1y);
-                Console.WriteLine("Random1-Y:" + rn1x + "X:" + rn1y);
-                Console.WriteLine("Random2-Y:" + rn2y + "X:" + rn2x);
-                Console.WriteLine("Random3-Y:" + rn3y + "X:" + rn3x);
-                //pole.Sea(rn1x, rn1y, rn2x, rn2y, rn3x, rn3y);
+                
+
+                
+                Console.Clear();
+                Console.WriteLine("Pole Player1: ");
+                pole.Player1Sea(player1rn1x, player1rn1y, player1rn2x, player1rn2y, player1rn3x, player1rn3y);
                 Console.WriteLine("Útočí Player2:");
                 Console.WriteLine("attack Zadej X:");
                 hitxs = Console.ReadLine();
                 Console.WriteLine("attack Zadej Y:");
+                Console.WriteLine("Random1-Y:" + player1rn1x + "X:" + player1rn1y);
+                Console.WriteLine("Random2-Y:" + player1rn2x + "X:" + player1rn2y);
+                Console.WriteLine("Random3-Y:" + player1rn3x + "X:" + player1rn3y);
                 hitys = Console.ReadLine();
+
                 int hitx = Convert.ToInt32(hitxs);
                 int hity = Convert.ToInt32(hitys);
                 Console.WriteLine("X:" + hitx + "Y" + hity);
-                pole.Seaattack(hity, hitx, rn1y, rn1x,rn1y, rn1x, rn2y, rn2x, rn3y, rn3x);
+                Console.Clear();
+                pole.Player1Seaattack(hity, hitx, player1rn1y, player1rn1x, player1rn1y, player1rn1x, player1rn2y, player1rn2x, player1rn3y, player1rn3x);
                 Console.WriteLine("--------------------------");
-                /*Console.WriteLine("Random1-Y:" + rn1x + "X:" + rn1y);
-                Console.WriteLine("Random2-Y:" + rn2x + "X:" + rn2y);
-                Console.WriteLine("Random3-Y:" + rn3x + "X:" + rn3y);*/
-                //ship.EnemyAttack(hitx, hity, rn1x, rn1y);
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Pole Player2: ");
+                pole.Player2Sea(player2rn1x, player2rn1y, player2rn2x, player2rn2y, player2rn3x, player2rn3y);
+                Console.WriteLine("Útočí Player1:");
+                Console.WriteLine("attack Zadej X:");
+                hitxs = Console.ReadLine();
+                Console.WriteLine("attack Zadej Y:");
+                hitys = Console.ReadLine();
+                int hitx2 = Convert.ToInt32(hitxs);
+                int hity2 = Convert.ToInt32(hitys);
+                Console.WriteLine("X:" + hitx2 + "Y" + hity2);
+                Console.WriteLine("Pole Player2: ");
+                pole.Player2Seaattack(hity2, hitx2, player2rn1y, player2rn1x, player2rn1y, player2rn1x, player2rn2y, player2rn2x, player2rn3y, player2rn3x);
+                Console.WriteLine("Random1-Y:" + player1rn1x + "X:" + player1rn1y);
+                Console.WriteLine("Random2-Y:" + player1rn2x + "X:" + player1rn2y);
+                Console.WriteLine("Random3-Y:" + player1rn3x + "X:" + player1rn3y);
+                //ship.EnemyAttack(hitx, hity, rn1x, rn1y);*/
             } while (5 == 5); 
         } 
         }
