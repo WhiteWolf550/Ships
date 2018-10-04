bc025a88aef04a274ebc05e17ea09d46fe1f8e88
WhiteWolf <viskot007@gmail.com>
2018-10-04 23:21:48 +0200

    new

--- Witcher/Witcher/Witcher/Program.cs
+++ Witcher/Witcher/Witcher/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,13 +10,7 @@ namespace Witcher
     {
         static void Main(string[] args)
         {
-            SoundPlayer player = new SoundPlayer(@"E:\C#\Witcher\Believe.wav");
-            SoundPlayer geralt = new SoundPlayer(@"E:\C#\Witcher\geralt.wav");
-            player.Play();
-            Console.ReadLine();
-            Console.WriteLine("Geralt");
-            geralt.Play();
-            Console.ReadLine();
+
         }
     }
 }
